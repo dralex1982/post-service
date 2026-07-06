896673b122b3386910037a40db8ed4d9f1046503
dralex1982 <dralex1982@gmail.com>
2026-07-06 17:12:39 +0300

    init. add postService, postController, postRoutes

---
+++ src/routes/postRoutes.js
@@ -0,0 +1,15 @@
+import {Router} from 'express';
+
+import * as controller from '../controller/postController.js';
+
+const router = Router();
+
+router.post('/post', controller.addPost)
+router.get('/post/:postId', controller.findPostById)
+router.patch('/post/:postId/like', controller.addLike)
+router.get('/posts/author/:user', controller.findPostsByAuthor)
+router.patch('/post/:postId/comment/:commenter', controller.addComment)
+router.delete('/post/:postId', controller.deletePost)
+router.get('/posts/tags', controller.findPostsByTags)
+router.get('/posts/period', controller.findPostsByPeriod)
+router.patch('/post/:postId', controller.updatePost)
---
+++ src/service/postService.js
@@ -0,0 +1,10 @@
+export const addPost = (user, post) => {}
+export const findPostById = (postId) => {}
+export const addLike = (postId) => {}
+export const findPostByAuthor = (user) => {}
+export const addComment = (postId, commenter, comment) => {}
+export const deletePost = (postId) => {}
+export const findPostsByTags = (tags) => {}
+export const findPostsByPeriod = (dateFrom, dateTo) => {}
+export const updatePost = (postId, data) => {}
+
