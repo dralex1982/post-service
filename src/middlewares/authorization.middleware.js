31b81f847622d6e0478277cac378d19f999e2873
Aleksei Zhuravlev <dralex1982@gmail.com>
2026-07-23 19:37:16 +0300

    add authorization for posts

--- src/middlewares/authorization.middleware.js
+++ src/middlewares/authorization.middleware.js
@@ -1,3 +1,5 @@
+import {findPostById} from "../services/postService.js";
+
 export const hasRole = role => (req, res, next) => {
     req.principal.roles.includes(role.toUpperCase().trim())
         ? next() : res.status(403).send({message: 'Access denied'});
@@ -10,5 +12,19 @@ export const isLogin = (paramName) => (req, res, next) => {
 
 export const isLoginOrAdmin = () => (req, res, next) => {
     req.principal.username === req.params.user || req.principal.roles.includes('ADMIN')
+        ? next() : res.status(403).send({message: 'Access denied'});
+}
+
+export const isPostAuthor = (postIdParam = 'postId') => async (req, res, next) => {
+    const postId = req.params[postIdParam];
+    const post = await findPostById(postId);
+    return post.author === req.principal.username
+        ? next() : res.status(403).send({message: 'Access denied'});
+}
+
+export const isPostAuthorOrHasRole = (postIdParam = 'postId', role) => async (req, res, next) => {
+    const postId = req.params[postIdParam];
+    const post = await findPostById(postId);
+    return post.author === req.principal.username || req.principal.roles.includes(role.toUpperCase().trim())
         ? next() : res.status(403).send({message: 'Access denied'});
 }
--- src/routes/authorization.routes.js
+++ src/routes/authorization.routes.js
@@ -1,14 +1,21 @@
 import {Router} from "express";
-import {hasRole, isLogin, isLoginOrAdmin} from "../middlewares/authorization.middleware.js";
+import {
+    hasRole,
+    isLogin,
+    isLoginOrAdmin,
+    isPostAuthor,
+    isPostAuthorOrHasRole
+} from "../middlewares/authorization.middleware.js";
 import {ADMIN} from "../configuration/constants.js";
 
 const router = Router();
 
 router.all('/account/user/:user/role/:role', hasRole(ADMIN))
-router.patch('/account/user/:user', isLogin("user"))
+router.patch(['/account/user/:user', '/forum/post/:postId/comment/:user'], isLogin("user"))
 router.delete('/account/user/:user', isLoginOrAdmin("user"))
 
 router.post('/forum/post/:author', isLogin("author"))
-router.patch('/forum/post/:postId/comment/:author', isLogin("author"))
+router.patch('/forum/post/:postId', isPostAuthor('postId'))
+router.delete('/forum/post/:postId', isPostAuthorOrHasRole('postId', 'MODERATOR'))
 
 export default router
